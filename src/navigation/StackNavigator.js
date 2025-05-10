7a66d27463b39547d7ad38ee17bb70915c3c8555
Vonky <deywidbraga14@gmail.com>
2025-05-09 23:01:30 -0300

    Tô corrigindo alguns bugs ainda, a coisa ta feia :(

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -16,16 +16,7 @@ export default function StackNavigator() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
+        
         <Stack.Screen
           name="Admin"
           component={AdminScreen}
